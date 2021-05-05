a6766e70bc396501160cd8ebef5a95e11f93d0ae
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2021-05-05 21:54:08 +0200

    Add index and package.json instructions about it

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Operators
+export { flatMap } from './operators/flatMap';
+export { flatMapFormer, flatMapLatter } from './operators/flatMapCoordinate';
+export { flatZipMap } from './operators/flatZipMap';
+export { listMap, flatListMap, listFlatMap, flatListFlatMap } from './operators/listMap';
+export { mapFormer, mapLatter } from './operators/mapCoordinate';
+export { projectToFormer, projectToLatter, projectTo } from './operators/projectTo';
+export { zipMap } from './operators/zipMap';
