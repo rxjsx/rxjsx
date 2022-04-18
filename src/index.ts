93f962e748e5b680f83d710bf1de50be7f77806f
SchweizS <18433748+SchweizS@users.noreply.github.com>
2022-04-19 00:14:38 +0200

    Add rxjs7 aliases
    Fixes: #22

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 // Operators
 export { flatMap } from './operators/flatMap';
+export { flatMap as mergeMap } from './operators/flatMap';
 export { flatMapFormer, flatMapLatter } from './operators/flatMapCoordinate';
+export { flatMapFormer as mergeMapFormer, flatMapLatter as mergeMapLatter } from './operators/flatMapCoordinate';
 export { flatZipMap } from './operators/flatZipMap';
+export { flatZipMap as mergeZipMap } from './operators/flatZipMap';
 export { listMap, flatListMap, listFlatMap, flatListFlatMap } from './operators/listMap';
+export { flatListMap as mergeListMap, flatListFlatMap as mergeListFlatMap } from './operators/listMap';
 export { mapFormer, mapLatter } from './operators/mapCoordinate';
 export { projectToFormer, projectToLatter, projectTo } from './operators/projectTo';
 export { zipMap } from './operators/zipMap';
